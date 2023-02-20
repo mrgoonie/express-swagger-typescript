95fb9373f26abc15b5b198948af6b5159f03cc5b
digitop-duynguyen <digitop-duynguyen>
2023-02-20 19:51:35 +0700

    add working example

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -3,6 +3,7 @@ import type { NextFunction, Request, Response } from "express";
 import PingService from "../services/ping.service";
 import { Get, Route } from "tsoa/dist";
 
+// This wouldn't work
 interface ResponseObject<T> {
 	status: number;
 	data: T | any;
@@ -18,3 +19,22 @@ export default class PingController {
 		return result;
 	}
 }
+
+/**
+ * This will work normally:
+ */
+// interface ResponseObject {
+// 	status: number;
+// 	data: PingEntity;
+// }
+
+// @Route("ping")
+// export default class PingController {
+// 	@Get()
+// 	async getMessage() {
+// 		const svc = new PingService();
+// 		const data = await svc.getMessage();
+// 		let result: ResponseObject = { status: 1, data };
+// 		return result;
+// 	}
+// }
